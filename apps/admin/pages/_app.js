5233229461c2363224b945d6e5018745d862e6fa
vaishali <vhr1289@gmail.com>
2021-01-18 12:42:17 +0530

    Admin Add theming support material UI index.js , _app.js Admin - Added theming support material UI Added ThemeProvider and material ui styles in dependencies

--- apps/admin/pages/_app.js
+++ apps/admin/pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from '../utils/theme.js';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+    return (<ThemeProvider theme={theme}>
+                <Component {...pageProps}/>
+            </ThemeProvider>)
 }
 
 export default MyApp
---
+++ apps/admin/utils/theme.js
@@ -0,0 +1,8 @@
+const theme = {
+    palette: {
+        primary:{
+            Color : '#3f51b5',
+        },
+    },
+}
+export default theme;
