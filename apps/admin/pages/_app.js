c19c2d61b3a502c86370adeb42db6318ceb9cd40
aman shah <amanjayshah@gmail.com>
2021-01-18 13:03:17 +0530

    Fix theming

--- apps/admin/pages/_app.js
+++ apps/admin/pages/_app.js
@@ -1,12 +1,15 @@
 import '../styles/globals.css'
 import { ThemeProvider } from '@material-ui/core/styles';
-import theme from '../utils/theme.js';
+import theme from '../utils/theme';
+
 
 function MyApp({ Component, pageProps }) {
 
-    return (<ThemeProvider theme={theme}>
-                <Component {...pageProps}/>
-            </ThemeProvider>)
+    return (
+      <ThemeProvider theme={theme}>
+          <Component {...pageProps}/>
+      </ThemeProvider>
+    );
 }
 
 export default MyApp
