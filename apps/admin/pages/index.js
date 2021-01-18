3a326d6d1202faf6ad4ecc417437c31b8c141f73
vaishali <vhr1289@gmail.com>
2021-01-18 12:42:17 +0530

    Admin Add theming support material UI index.js , _app.js Admin - Added theming support material UI Added ThemeProvider and material ui styles in dependencies

--- apps/admin/pages/index.js
+++ apps/admin/pages/index.js
@@ -1,27 +1,16 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 import Parse from 'parse';
-import { makeStyles } from '@material-ui/core/styles';
 
 Parse.initialize("answerbookApi");
 Parse.serverURL = 'http://localhost:9000/parse';
 
-const useStyles = makeStyles({
-  root: {
-    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-    border: 0,
-    borderRadius: 3,
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: 'white',
-    height: 48,
-    padding: '0 30px',
-  },
-});
 
 export default function Home() {
-  const classes = useStyles();
+
   return (
     <div className={styles.container}>
       <Head>
