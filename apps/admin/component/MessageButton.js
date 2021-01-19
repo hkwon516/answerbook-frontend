1b4159dcdfaab02503096058d33e002b2fcf5a0b
vishal shah <shahvishal712@gmail.com>
2021-01-19 18:33:40 +0530

    add formating

--- apps/admin/component/MessageButton.js
+++ apps/admin/component/MessageButton.js
@@ -1,25 +1,23 @@
-import { withSnackbar } from 'notistack';
-import { Button, withStyles } from '@material-ui/core';
+import { withSnackbar } from "notistack";
+import { Button, withStyles } from "@material-ui/core";
 import styles from "../styles/Home.module.css";
-import { Component } from 'react';
+import { Component } from "react";
 
 class MessageButton extends Component {
-    handleClick = () => {
-        // Avoid material-ui warnings. more info: https://material-ui.com/style/typography/#migration-to-typography-v2
-        // eslint-disable-next-line no-underscore-dangle
-        window.__MUI_USE_NEXT_TYPOGRAPHY_VARIANTS__ = true;
-        this.props.enqueueSnackbar('Welcome to admin', { variant: 'success' });
-    };
+  handleClick = () => {
+    // Avoid material-ui warnings. more info: https://material-ui.com/style/typography/#migration-to-typography-v2
+    // eslint-disable-next-line no-underscore-dangle
+    window.__MUI_USE_NEXT_TYPOGRAPHY_VARIANTS__ = true;
+    this.props.enqueueSnackbar("Welcome to admin", { variant: "success" });
+  };
 
-    render() {
-        return (
-            <Button color="primary"
-                variant="contained"
-                onClick={this.handleClick}>
-                Click here.
-            </Button>
-        );
-    }
+  render() {
+    return (
+      <Button color="primary" variant="contained" onClick={this.handleClick}>
+        Click here.
+      </Button>
+    );
+  }
 }
 
 export default withStyles(styles)(withSnackbar(MessageButton));
