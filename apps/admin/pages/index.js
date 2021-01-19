1ba1d7279bf2d8b1698eb168d984876bdfc84b79
vaishali <vhr1289@gmail.com>
2021-01-19 11:02:56 +0530

    Admin UI for Singup index.js , signUp.js Add UI for Signup Added UI for signup page , add new folder inside admin , give url to signup page in index.js

--- apps/admin/pages/index.js
+++ apps/admin/pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { Button } from '@material-ui/core';
+import { useRouter } from "next/router";
 
 import Parse from 'parse';
 
@@ -9,6 +10,7 @@ Parse.serverURL = 'http://localhost:9000/parse';
 
 
 export default function Home() {
+  const router = useRouter();
 
   return (
     <div className={styles.container}>
@@ -21,7 +23,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>Answer Book Admin</h1>
-        <Button color="primary">Hello World</Button>
+        <Button color="primary" onClick={() => router.push('/anon/signUp')}>Sign Up</Button>
       </main>
 
       
