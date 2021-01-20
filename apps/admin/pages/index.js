839058765fa7ee9607aec8518be1e4c1053ffe34
vishal shah <shahvishal712@gmail.com>
2021-01-20 18:26:09 +0530

    admin - Parse - refactoring

--- apps/admin/pages/index.js
+++ apps/admin/pages/index.js
@@ -5,29 +5,23 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { withSnackbar } from "notistack";
 
-import Parse from "parse";
-
-Parse.initialize("answerbookApi");
-Parse.serverURL = "http://localhost:9000/parse";
-
 export default function Home(props) {
   const router = useRouter();
-    return (
-        <div className={styles.container}>
-           
-            <main className={styles.main}>
-                <h1 className={styles.title}>{props.t("welcomeToAnswerbook")}</h1>
-                <Button color="primary">{props.t("hello")}</Button>
-                <Button color="primary" onClick={() => router.push("/signup")}>
-                    Sign Up
-                </Button>
-                <Button color="primary" onClick={() => router.push("/login")}>
-                    Login
-                </Button>
-                <Link href="/" locale="ko">
-                    <a>Translate to Korea</a>
-                </Link>
-            </main>
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>{props.t("welcomeToAnswerbook")}</h1>
+        <Button color="primary">{props.t("hello")}</Button>
+        <Button color="primary" onClick={() => router.push("/signup")}>
+          Sign Up
+        </Button>
+        <Button color="primary" onClick={() => router.push("/login")}>
+          Login
+        </Button>
+        <Link href="/" locale="ko">
+          <a>Translate to Korea</a>
+        </Link>
+      </main>
+    </div>
+  );
 }
