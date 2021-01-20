2858bee23a1794ea3975203fd08d3a5273bd33f4
vishal shah <shahvishal712@gmail.com>
2021-01-20 16:32:34 +0530

    admin - withApp - refactoring

--- apps/admin/utils/withApp.js
+++ apps/admin/utils/withApp.js
@@ -1,32 +1,26 @@
-// This function takes a component...
 import React from "react";
 import { useRouter } from "next/router";
 import getTranslation from "../utils/locales";
 import { useSnackbar } from "notistack";
 
-export default function withApp(WrappedComponent) {
-  return function (props) {
+const withApp = (WrappedComponent) => {
+  const ExtendedWithApp = (props) => {
     const snackbar = useSnackbar();
 
     const router = useRouter();
     const t = getTranslation(router.locale);
 
     const showSuccess = (message) => {
-      console.log(snackbar);
-      if (snackbar) {
-        snackbar.enqueueSnackbar(message);
-      } else {
-        console.log(message);
-      }
+      snackbar.enqueueSnackbar(message);
     };
+
     const showError = (message) => {
-      if (snackbar) {
-        snackbar.enqueueSnackbar(message);
-      } else {
-        console.log(message);
-      }
+      snackbar.enqueueSnackbar(message);
     };
 
     return <WrappedComponent t={t} showSuccess={showSuccess} showError={showError} {...props} />;
   };
-}
+  return ExtendedWithApp;
+};
+
+export default withApp;
