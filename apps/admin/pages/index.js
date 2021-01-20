e6883e2fde4d260019ed9274d23441e364bc0fc2
vishal shah <shahvishal712@gmail.com>
2021-01-20 16:32:34 +0530

    admin - withApp - refactoring

--- apps/admin/pages/index.js
+++ apps/admin/pages/index.js
@@ -1,27 +1,16 @@
 import React from "react";
-import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { Button } from "@material-ui/core";
 import Link from "next/link";
-import { useRouter } from "next/router";
-import { withSnackbar } from "notistack";
 
 import Parse from "parse";
-import MessageButton from "../component/MessageButton";
 
 Parse.initialize("answerbookApi");
 Parse.serverURL = "http://localhost:9000/parse";
 
-export default function Home(props) {
+const Home = (props) => {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Answerbook Admin</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-      </Head>
-
       <main className={styles.main}>
         <h1 className={styles.title}>{props.t("welcomeToAnswerbook")}</h1>
         <Button color="primary">{props.t("hello")}</Button>
@@ -42,4 +31,6 @@ export default function Home(props) {
       </main>
     </div>
   );
-}
+};
+
+export default Home;
