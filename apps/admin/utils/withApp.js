2ea4194cde548027f7e57d494309bf5afecd519d
vishal shah <shahvishal712@gmail.com>
2021-01-20 10:49:47 +0530

    use usesnackbar of notistack

--- apps/admin/utils/withApp.js
+++ apps/admin/utils/withApp.js
@@ -2,13 +2,30 @@
 import React from "react";
 import { useRouter } from "next/router";
 import getTranslation from "../utils/locales";
+import { useSnackbar } from "notistack";
 
 export default function withApp(WrappedComponent) {
   // ...and returns another component...
   return function (props) {
+    const { enqueueSnackbar } = useSnackbar();
+
     const router = useRouter();
     const t = getTranslation(router.locale);
 
-    return <WrappedComponent t={t} {...props} />;
+    const showSuccess = (message) => {
+      enqueueSnackbar(message);
+    };
+    const showError = (message) => {
+      enqueueSnackbar(message);
+    };
+
+    return (
+      <WrappedComponent
+        t={t}
+        showSuccess={showSuccess}
+        showError={showError}
+        {...props}
+      />
+    );
   };
 }
