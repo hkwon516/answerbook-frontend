21afadec2985dbe6cb20013dd13afba97e07ce23
vishal shah <shahvishal712@gmail.com>
2021-01-20 13:26:11 +0530

    use useSnackbar for notistack

--- apps/admin/component/MessageButton.js
+++ apps/admin/component/MessageButton.js
@@ -1,20 +1,20 @@
-import { withSnackbar } from "notistack";
 import { Button, withStyles } from "@material-ui/core";
 import styles from "../styles/Home.module.css";
 import { Component } from "react";
+import withApp from "../utils/withApp";
 
 class MessageButton extends Component {
-  handleClick = () => {
-    this.props.enqueueSnackbar("Welcome to admin", { variant: "success" });
-  };
+    // handleClick = () => {
+    //     this.props.enqueueSnackbar("Welcome to admin", { variant: "success" });
+    // };
 
-  render() {
-    return (
-      <Button color="primary" variant="contained" onClick={this.handleClick}>
-        Click here.
-      </Button>
-    );
-  }
+    render() {
+        return (
+            <Button color="primary" variant="contained" onClick={(e) => this.props.showSuccess("Success Message")}>
+                Click here.
+            </Button>
+        );
+    }
 }
 
-export default withStyles(styles)(withSnackbar(MessageButton));
+export default withStyles(styles)(withApp(MessageButton));
--- apps/admin/utils/withApp.js
+++ apps/admin/utils/withApp.js
@@ -5,27 +5,22 @@ import getTranslation from "../utils/locales";
 import { useSnackbar } from "notistack";
 
 export default function withApp(WrappedComponent) {
-  // ...and returns another component...
-  return function (props) {
-    const { enqueueSnackbar } = useSnackbar();
+    // ...and returns another component...
+    return function (props) {
+        const { enqueueSnackbar } = useSnackbar();
 
-    const router = useRouter();
-    const t = getTranslation(router.locale);
+        const router = useRouter();
+        const t = getTranslation(router.locale);
 
-    const showSuccess = (message) => {
-      enqueueSnackbar(message);
+        const showSuccess = (message, option) => {
+            enqueueSnackbar(message, option);
+            console.log(message, option);
+        };
+        const showError = (message) => {
+            // enqueueSnackbar(message);
+            console.log(message);
+        };
+
+        return <WrappedComponent t={t} showSuccess={showSuccess} showError={showError} {...props} />;
     };
-    const showError = (message) => {
-      enqueueSnackbar(message);
-    };
-
-    return (
-      <WrappedComponent
-        t={t}
-        showSuccess={showSuccess}
-        showError={showError}
-        {...props}
-      />
-    );
-  };
 }
