fd6cf953c3e0cb5832e4ad2e8b159a7410cb0e4c
Kishan Thobhani <thobhanikishan@gmail.com>
2021-01-21 12:17:09 +0530

    admin - layout, theme - refactoring

--- apps/admin/component/generic/InputComponent.js
+++ apps/admin/component/generic/InputComponent.js
@@ -1,12 +1,18 @@
 import { TextField } from "@material-ui/core";
 
 const InputComponent = ({ formik, ...props }) => {
-  
   const hasError = formik.touched[props.name] && formik.errors[props.name] ? true : false;
+  let extendedProps = { ...props };
+  if (!props.margin) {
+    extendedProps = { ...extendedProps, margin: hasError ? "dense" : "normal" };
+  }
+
+  if (!props.variant) {
+    extendedProps = { ...extendedProps, variant: "filled" };
+  }
   return (
     <TextField
-      {...props}
-      margin={hasError ? "dense" : "normal"}
+      {...extendedProps}
       onChange={formik.handleChange}
       value={formik.values[props.name]}
       onBlur={formik.handleBlur}
--- apps/admin/pages/login.js
+++ apps/admin/pages/login.js
@@ -1,15 +1,10 @@
 import React from "react";
-import { LockOutlined } from "@material-ui/icons";
-import { Typography, Avatar, Button, Link, Grid, makeStyles, Container, Box } from "@material-ui/core";
+
+import { Typography, Button, Grid, Box } from "@material-ui/core";
 import InputComponent from "../component/generic/InputComponent";
 import { useFormik } from "formik";
 import * as yup from "yup";
-
-const useStyles = makeStyles((theme) => ({
-  wrapper: {
-    height: "100vh",
-  },
-}));
+import LinkComponent from "../component/generic/LinkComponent";
 
 export default function Login() {
   const formik = useFormik({
@@ -26,95 +21,55 @@ export default function Login() {
     },
   });
 
-  const classes = useStyles();
-
   return (
-    <Container component="main" maxWidth="xs">
-      <Grid container alignItems="center" className={classes.wrapper}>
-        <Grid item>
-          <form noValidate onSubmit={formik.handleSubmit}>
-            <Grid container justify={"center"}>
-              <Grid item xs={12}>
-                <Box mb={3}>
-                  <Grid container direction="column" alignItems="center">
-                    <Grid item>
-                      <Box mb={1}>
-                        <Avatar>
-                          <LockOutlined />
-                        </Avatar>
-                      </Box>
-                    </Grid>
-                    <Grid item>
-                      <Typography component="h1" variant="h5">
-                        Login
-                      </Typography>
-                    </Grid>
-                  </Grid>
-                </Box>
-              </Grid>
-
-              <Grid item xs={12}>
-                <InputComponent
-                  variant="outlined"
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                  autoFocus
-                  formik={formik}
-                />
-              </Grid>
-
-              <Grid item xs={12}>
-                <InputComponent
-                  variant="outlined"
-                  margin="normal"
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="current-password"
-                  formik={formik}
-                />
-              </Grid>
-
-              <Grid item xs={12}>
-                <Box mt={3}>
-                  <Button type="submit" fullWidth variant="contained">
-                    Login
-                  </Button>
-                </Box>
-              </Grid>
-
-              <Grid item xs>
-                <Box mt={2}>
-                  <Link href="forgot/password" variant="body2">
-                    Forgot password?
-                  </Link>
-                </Box>
-
-                <Box>
-                  <Link href="forgot/email" variant="body2">
-                    Forgot Email?
-                  </Link>
-                </Box>
-              </Grid>
-              <Grid item>
-                <Box mt={2}>
-                  <Link href="/signup" variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
-                </Box>
-              </Grid>
-            </Grid>
-          </form>
+    <form noValidate onSubmit={formik.handleSubmit}>
+      <Grid container justify={"center"}>
+        <Grid item xs={12}>
+          <Box mb={2} textAlign="left">
+            <Typography variant="h4">Log In</Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={12}>
+          <InputComponent required fullWidth id="email" label="Email Address" name="email" autoComplete="email" autoFocus formik={formik} />
+        </Grid>
+        <Grid item xs={12}>
+          <InputComponent
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            formik={formik}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Box textAlign="right" mt={2}>
+            <Box>
+              <LinkComponent href="forgot/password">Forgot password?</LinkComponent>
+            </Box>
+            <Box>
+              <LinkComponent href="forgot/email">Forgot Email?</LinkComponent>
+            </Box>
+          </Box>
+        </Grid>
+        <Grid item xs={12}>
+          <Box mt={3}>
+            <Button color="secondary" type="submit" fullWidth variant="contained">
+              Login
+            </Button>
+          </Box>
+        </Grid>
+        <Grid item xs={12}>
+          <Box mt={2} textAlign="center">
+            <Typography variant="body1">
+              Don't have an account? <LinkComponent href="/signup">Sign Up</LinkComponent>
+            </Typography>
+          </Box>
         </Grid>
       </Grid>
-    </Container>
+    </form>
   );
 }
