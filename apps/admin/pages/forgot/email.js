b79f1beb788b4b6325505f7755ef9f656e12c7da
vaishali <vhr1289@gmail.com>
2021-01-21 19:10:58 +0530

    admin -userLayout -add button and popover on it - fix ui of forgot password and email

--- apps/admin/pages/forgot/email.js
+++ apps/admin/pages/forgot/email.js
@@ -1,96 +1,67 @@
 import React from "react";
-import {
-  Typography,
-  Button,
-  Grid,
-  makeStyles,
-  Container,
-  Box,
-} from "@material-ui/core";
+import { Typography, Button, Grid, makeStyles, Container, Box } from "@material-ui/core";
 import InputComponent from "../../component/generic/InputComponent";
 import { useFormik } from "formik";
 import * as yup from "yup";
 
-const useStyles = makeStyles((theme) => ({
-  wrapper: {
-    height: "100vh",
-  },
-}));
 
-export default function forgotEmail() {
+export default function forgotEmail(props) {
   const formik = useFormik({
     initialValues: {
       name: "",
       phoneNumber: "",
     },
     validationSchema: yup.object().shape({
-      name: yup.string().required(),
-      phoneNumber: yup.number().required(),
+      name: yup.string().required(props.translate("nameRequired")),
+      phoneNumber: yup.number(props.translate("phoneNumberValidate")).required(props.translate("phoneNumberRequired")),
     }),
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
     },
   });
 
-  const classes = useStyles();
 
   return (
-    <Container component="main" maxWidth="xs">
-      <Grid container alignItems="center" className={classes.wrapper}>
-        <Grid item>
-          <form noValidate onSubmit={formik.handleSubmit}>
-            <Grid container justify={"center"}>
-              <Grid item xs={12}>
-                <Box mb={3}>
-                  <Grid container direction="column" alignItems="center">
-                    <Grid item>
-                      <Typography component="h1" variant="h5">
-                      Please write down your name and your phone number.
-                      </Typography>
-                    </Grid>
-                  </Grid>
-                </Box>
-              </Grid>
-
-              <Grid item xs={12}>
-                <InputComponent
-                  autoComplete="name"
-                  name="name"
-                  variant="outlined"
-                  required
-                  fullWidth
-                  id="name"
-                  label="Name"
-                  autoFocus
-                  formik={formik}
-                />
-              </Grid>
-
-              <Grid item xs={12}>
-                <InputComponent
-                  variant="outlined"
-                  margin="normal"
-                  required
-                  fullWidth
-                  name="phoneNumber"
-                  label="Phone Number"
-                  id="phoneNumber"
-                  autoComplete="Phone Number"
-                  formik={formik}
-                />
-              </Grid>
-
-              <Grid item xs={12}>
-                <Box mt={3}>
-                  <Button type="submit" fullWidth variant="contained">
-                    Next
-                  </Button>
-                </Box>
-              </Grid>
-            </Grid>
-          </form>
+    <form noValidate onSubmit={formik.handleSubmit}>
+      <Grid container justify={"center"}>
+        <Grid item xs={12}>
+          <Box mb={2} textAlign="left">
+            <Typography variant="h4">Forgot Email</Typography>
+            <Typography variant="subtitle1">{props.translate("pageForgotEmailSubtitle")}</Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={12}>
+          <InputComponent
+            required
+            fullWidth
+            id="name"
+            label={props.translate("name")}
+            name="name"
+            autoComplete="name"
+            autoFocus
+            formik={formik}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <InputComponent
+            required
+            fullWidth
+            id="email"
+            label={props.translate("emailAddress")}
+            name="email"
+            autoComplete="email"
+            autoFocus
+            formik={formik}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Box mt={3}>
+            <Button color="secondary" type="submit" fullWidth variant="contained">
+              {props.translate("btnOk")}
+            </Button>
+          </Box>
         </Grid>
       </Grid>
-    </Container>
+    </form>
   );
 }
