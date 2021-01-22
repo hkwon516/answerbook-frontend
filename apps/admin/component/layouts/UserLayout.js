a721ceba470c3f1e2fceda7bee40c9bae6f16f65
Hannah Kwon <kteh0516@gmail.com>
2021-01-22 19:10:38 +0530

    admin - app - disable ssr for authenticated user

--- apps/admin/component/layouts/UserLayout.js
+++ apps/admin/component/layouts/UserLayout.js
@@ -20,15 +20,9 @@ import * as yup from "yup";
 import Parse from "parse";
 import InputComponent from "../../component/generic/InputComponent";
 
-const useStyles = makeStyles((theme) => ({
-  typography: {
-    padding: theme.spacing(2),
-  },
-}));
+const useStyles = makeStyles((theme) => ({}));
 
 const UserLayout = (props) => {
-  const classes = useStyles();
-
   const [anchorEl, setAnchorEl] = React.useState(null);
 
   const handleClick = (event) => {
@@ -42,121 +36,74 @@ const UserLayout = (props) => {
   const open = Boolean(anchorEl);
   const id = open ? "simple-popover" : undefined;
 
-  console.log(props.theme.palette.primary);
-
-  const formik = useFormik({
-    initialValues: {
-      name: "",
-      email: "",
-      phoneNumber: "",
-      password: "",
-      passwordConfirm: "",
-      position: "teacher",
-      academyName: "",
-      companyEmail: "",
-      purpose: "",
-    },
-    validationSchema: yup.object().shape({
-      name: yup.string().required(props.translate("nameRequired")),
-      email: yup.string().required(props.translate("emailRequired")).email(props.translate("emailValidate")),
-      phoneNumber: yup.number(props.translate("phoneNumberValidate")).required(props.translate("phoneNumberRequired")),
-      password: yup.string().required(props.translate("passwordRequired")),
-      passwordConfirm: yup
-        .string()
-        .required(props.translate("passwordConfirmationRequired"))
-        .oneOf([yup.ref("password")], props.translate("passwordValidate")),
-    }),
-
-    onSubmit: async (values) => {
-      try {
-        const user = new Parse.User();
-        user.set("name", values.name);
-        user.set("username", values.email);
-        user.set("email", values.email);
-        user.set("password", values.password);
-        user.set("phoneNumber", values.phoneNumber);
-        user.set("position", values.position);
-        user.set("academyName", values.academyName);
-        user.set("companyEmail", values.companyEmail);
-        user.set("purpose", values.purpose);
-
-        await user.signUp();
-        props.showSuccess(props.translate("signupSuccess"));
-        router.push("/login");
-      } catch (error) {
-        props.showError(error.message);
-      }
-    },
-  });
-
-  return props.isBrowser ? (
-    <Grid container fullWidth={true}>
-      <Grid item xs={12} md={2}>
-        <Box bgcolor="secondary.main" height="100vh">
-          <Grid container direction="column" justify="space-between" style={{ height: "100%" }}>
-            <Box p={2}>
-              <Grid item>
-                <Grid container>
-                  <Grid item xs={12}>
-                    logo
-                  </Grid>
-                  <Grid item xs={12}>
-                    naivgation
-                  </Grid>
-                </Grid>
-              </Grid>
-            </Box>
-            <Grid item>
-              <Box p={2} onClick={handleClick} fullWidth={true} bgcolor="secondary.dark" style={{ cursor: "pointer" }}>
-                <Popover
-                  id={id}
-                  open={open}
-                  anchorEl={anchorEl}
-                  onClose={handleClose}
-                  anchorOrigin={{
-                    vertical: "center",
-                    horizontal: "left",
-                  }}
-                  transformOrigin={{
-                    vertical: "bottom",
-                    horizontal: "left",
-                  }}
-                  elevation={1}
-                >
+  return (
+    <>
+      <Grid container fullWidth={true}>
+        <Grid item xs={12} md={2}>
+          <Box bgcolor="secondary.main" height="100vh">
+            <Grid container direction="column" justify="space-between" style={{ height: "100%" }}>
+              <Box p={2}>
+                <Grid item>
                   <Grid container>
                     <Grid item xs={12}>
-                      <List>
-                        <ListItem>
-                          <ListItemText secondary="Sign Out" />
-                        </ListItem>
-                      </List>
+                      logo
                     </Grid>
-                  </Grid>
-                </Popover>
-                <Grid container alignItems="center">
-                  <Grid item>
-                    <Avatar style={{ backgroundColor: props.theme.palette.secondary.light }} size="small">
-                      {props.user.get("name").split("")[0].toUpperCase()}
-                    </Avatar>
-                  </Grid>
-                  <Grid item>
-                    <Box ml={1}>
-                      <Typography variant="body2">{props.user.get("name")}</Typography>
-                      <Typography variant="caption">{props.user.getEmail()}</Typography>
-                    </Box>
+                    <Grid item xs={12}>
+                      naivgation
+                    </Grid>
                   </Grid>
                 </Grid>
               </Box>
+              <Grid item>
+                <Box p={2} onClick={handleClick} fullWidth={true} bgcolor="secondary.dark" style={{ cursor: "pointer" }}>
+                  <Popover
+                    id={id}
+                    open={open}
+                    anchorEl={anchorEl}
+                    onClose={handleClose}
+                    anchorOrigin={{
+                      vertical: "center",
+                      horizontal: "left",
+                    }}
+                    transformOrigin={{
+                      vertical: "bottom",
+                      horizontal: "left",
+                    }}
+                    elevation={1}
+                  >
+                    <Grid container>
+                      <Grid item xs={12}>
+                        <List>
+                          <ListItem>
+                            <ListItemText secondary="Sign Out" />
+                          </ListItem>
+                        </List>
+                      </Grid>
+                    </Grid>
+                  </Popover>
+                  <Grid container alignItems="center">
+                    <Grid item>
+                      <Avatar style={{ backgroundColor: props.theme.palette.secondary.light }} size="small">
+                        {props.user.get("name").split("")[0].toUpperCase()}
+                      </Avatar>
+                    </Grid>
+                    <Grid item>
+                      <Box ml={1}>
+                        <Typography variant="body2">{props.user.get("name")}</Typography>
+                        <Typography variant="caption">{props.user.getEmail()}</Typography>
+                      </Box>
+                    </Grid>
+                  </Grid>
+                </Box>
+              </Grid>
             </Grid>
-          </Grid>
-        </Box>
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={10}>
+          {props.children}
+        </Grid>
       </Grid>
-      <Grid item xs={12} md={10}>
-        {props.children}
-      </Grid>
-    </Grid>
-  ) : (
-    <></>
+    </>
   );
 };
 
--- apps/admin/pages/_app.js
+++ apps/admin/pages/_app.js
@@ -13,20 +13,26 @@ function MyApp({ Component, pageProps, ...props }) {
   const [isBrowser, setIsBrowser] = useState(false);
   const [user, setUser] = useState(parse.User.current());
 
+  const isAuthenticatedRoute = props.router.pathname.includes("user");
+
   useEffect(() => {
     setUser(parse.User.current());
     setIsBrowser(true);
-    // Remove the server-side injected CSS.
     const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles) {
       jssStyles.parentElement.removeChild(jssStyles);
     }
+  }, []);
 
-    console.log(props.router);
-    if (user && !props.router.pathname.includes("user")) {
+  useEffect(() => {
+    if (user && !isAuthenticatedRoute) {
       props.router.push("/user");
     }
-  }, []);
+  }, [user]);
+
+  if (isAuthenticatedRoute && !isBrowser) {
+    return <></>;
+  }
 
   const Layout = user ? UserLayout : AnonLayout;
   const ComponentExtended = withApp(Component);
