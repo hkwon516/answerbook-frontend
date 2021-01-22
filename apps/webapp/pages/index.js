2bbe01ed04f94442db69920307e6bf28c0b45f1e
vaishali <vhr1289@gmail.com>
2021-01-22 19:29:40 +0530

    webapp - signup page Ui - signup.js -created signup file added icon button and fix layout

--- apps/webapp/pages/index.js
+++ apps/webapp/pages/index.js
@@ -9,15 +9,9 @@ Parse.initialize("answerbookApi");
 Parse.serverURL = "http://localhost:9000/parse";
 
 export default function Home(props) {
+  console.log(props);
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Answerbook Webapp</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-      </Head>
-
       <main className={styles.main}>
         <h1 className={styles.title}>{props.translate("welcomeToAnswerbook")}</h1>
         <Button color="primary">{props.translate("hello")}</Button>
