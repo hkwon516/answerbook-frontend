469a1548e9196f36575d0a428e0bd9606ff816c7
vishal shah <shahvishal712@gmail.com>
2021-01-25 14:54:34 +0530

    admin - sign up - add validations for dynamic position fields

--- apps/admin/pages/signup.js
+++ apps/admin/pages/signup.js
@@ -36,6 +36,12 @@ export default function SignUp(props) {
         .string()
         .required(props.translate("pages.anon.signup.form.validation.passwordConfirmationRequired"))
         .oneOf([yup.ref("password")], props.translate("pages.anon.signup.form.validation.passwordValidate")),
+      academyName: yup.string().required(props.translate("pages.anon.signup.form.validation.academyNameRequired")),
+      companyEmail: yup
+        .string()
+        .required(props.translate("pages.anon.signup.form.validation.companyEmailRequired"))
+        .email(props.translate("pages.anon.signup.form.validation.comapnyEmailValidate")),
+      purpose: yup.string().required(props.translate("pages.anon.signup.form.validation.purposeRequired")),
     }),
 
     onSubmit: async (values, actions) => {
@@ -141,6 +147,7 @@ export default function SignUp(props) {
         {formik.values.position === "publisher" && (
           <Grid item xs={12}>
             <InputComponent
+              required
               fullWidth
               name="companyEmail"
               label={props.translate("pages.anon.signup.form.fields.companyEmail")}
@@ -155,6 +162,7 @@ export default function SignUp(props) {
         {formik.values.position === "etc" && (
           <Grid item xs={12}>
             <InputComponent
+              required
               fullWidth
               name="purpose"
               label={props.translate("pages.anon.signup.form.fields.purpose")}
