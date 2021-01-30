24dd26ab47b38aa4432c54ca6d592066175b80ff
vishal shah <shahvishal712@gmail.com>
2021-01-30 19:11:42 +0530

    Admin + webApp - merge admin and webapp

--- apps/admin/utils/withApp.js
+++ apps/admin/utils/withApp.js
@@ -11,6 +11,7 @@ import { ThemeProvider } from "@material-ui/core/styles";
 import { CssBaseline } from "@material-ui/core";
 import { SnackbarProvider } from "notistack";
 import Head from "next/head";
+import Cookies from "universal-cookie";
 
 const withUser = (WrappedComponent) => {
   class ExtendedWithUser extends React.Component {
@@ -34,7 +35,7 @@ const withUser = (WrappedComponent) => {
       if (user && !user.get("emailVerified")) {
         user = await user.fetch();
       }
-      this.setState({ user });
+      this.setState({ user, loading: false });
     };
 
     onLogout = async () => {
@@ -116,12 +117,34 @@ const withApp = (WrappedComponent) => {
       if (!contexts.user && isAuthenticatedRoute) {
         props.router.push("/");
       }
+
+      if (contexts.user) {
+        const userLocale = contexts.user.get("locale");
+        if (userLocale !== props.router.locale) {
+          changeLanguage(userLocale);
+        }
+      }
     }, [contexts.user]);
 
     const isMobile = props.width === "xs" || props.width === "sm";
 
-    const changeLanguage = (locale) => {
+    const cookies = new Cookies();
+
+    const setCookie = (key, value) => {
+      cookies.set(key, value);
+    };
+
+    const getCookie = (key) => {
+      return cookies.get(key);
+    };
+
+    const changeLanguage = async (locale) => {
+      setCookie("NEXT_LOCALE", locale);
+      contexts.user.set("locale", locale);
       props.router.push(props.router.pathname, props.router.pathname, { locale: locale });
+
+      const updatedUser = await contexts.user.save();
+      contexts.setUser(updatedUser);
     };
 
     const changePage = (pathname) => {
