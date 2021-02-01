03a6a5409eb0e163078e841587674134ad9aae26
Kishan Thobhani <thobhanikishan@gmail.com>
2021-02-01 13:06:37 +0530

    webapp - menu responsive

--- apps/webapp/pages/user/index.js
+++ apps/webapp/pages/user/index.js
@@ -1,11 +1,19 @@
-import { Button, Grid, Box, Tabs, Tab, IconButton } from "@material-ui/core";
+import { Button, Grid, Box, Tabs, Tab, IconButton, Container } from "@material-ui/core";
 import React from "react";
 import HomeIcon from "@material-ui/icons/Home";
 import CheckCircleOutlineIcon from "@material-ui/icons/CheckCircleOutline";
 import BookmarkIcon from "@material-ui/icons/Bookmark";
 import AssignmentIcon from "@material-ui/icons/Assignment";
 import CameraAltIcon from "@material-ui/icons/CameraAlt";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  scrollerOverride: {
+    overflow: "visible !important",
+  },
+});
 const Dashboard = (props) => {
+  const classes = useStyles();
   return (
     <div>
       <Grid container style={{ height: "100vh" }} direction="column" justify="space-between">
@@ -20,42 +28,51 @@ const Dashboard = (props) => {
         </Grid>
         <Grid item>
           <Box pb={2} bgcolor="primary.main">
-            <Tabs
-              classes={{ scroller: { overflow: "visible" } }}
-              style={{ overflow: "visible" }}
-              variant="fullWidth"
-              indicatorColor="secondary"
-              textColor="secondary"
-            >
-              <Tab
-                icon={<HomeIcon  style={{ color: "#fff" }} />}
-                style={{ fontWeight: 500, minHeight: 62, fontSize: 10, color: "#fff" }}
-                label="Home"
-              />
-              <Tab
-                icon={<CheckCircleOutlineIcon  style={{ color: "#fff" }} />}
-                style={{ fontWeight: 500, minHeight: 62, fontSize: 10, color: "#fff" }}
-                label="Grading"
-              />
-              <Tab
-                component={() => (
-                  <IconButton size="small" style={{ width: 52, height: 52, backgroundColor: props.theme.palette.secondary.main, zIndex: 1, top: -25 }}>
-                    <CameraAltIcon style={{ color: "#fff" }} />
-                  </IconButton>
-                )}
-                style={{ fontWeight: 500, minHeight: 62, fontSize: 10, color: "#fff" }}
-              />
-              <Tab
-                icon={<BookmarkIcon  style={{ color: "#fff" }} />}
-                style={{ fontWeight: 500, minHeight: 62, fontSize: 10, color: "#fff" }}
-                label="Notes"
-              />
-              <Tab
-                icon={<AssignmentIcon  style={{ color: "#fff" }} />}
-                style={{ fontWeight: 500, minHeight: 62, fontSize: 10, color: "#fff" }}
-                label="Today"
-              />
-            </Tabs>
+            <Container disableGutters maxWidth="md">
+              <Grid justify="center" container>
+                <Grid item xs={12}>
+                  <Tabs
+                    classes={{ scroller: classes.scrollerOverride }}
+                    style={{ overflow: "visible" }}
+                    variant="fullWidth"
+                    indicatorColor="secondary"
+                    textColor="secondary"
+                  >
+                    <Tab
+                      icon={<HomeIcon style={{ color: "#fff" }} />}
+                      style={{ fontWeight: 500, minHeight: 62, fontSize: 10, color: "#fff" }}
+                      label="Home"
+                    />
+                    <Tab
+                      icon={<CheckCircleOutlineIcon style={{ color: "#fff" }} />}
+                      style={{ fontWeight: 500, minHeight: 62, fontSize: 10, color: "#fff" }}
+                      label="Grading"
+                    />
+                    <Tab
+                      component={() => (
+                        <IconButton
+                          size="small"
+                          style={{ width: 52, height: 52, backgroundColor: props.theme.palette.secondary.main, zIndex: 1, top: -25 }}
+                        >
+                          <CameraAltIcon style={{ color: "#fff" }} />
+                        </IconButton>
+                      )}
+                      style={{ fontWeight: 500, minHeight: 62, fontSize: 10, color: "#fff" }}
+                    />
+                    <Tab
+                      icon={<BookmarkIcon style={{ color: "#fff" }} />}
+                      style={{ fontWeight: 500, minHeight: 62, fontSize: 10, color: "#fff" }}
+                      label="Notes"
+                    />
+                    <Tab
+                      icon={<AssignmentIcon style={{ color: "#fff" }} />}
+                      style={{ fontWeight: 500, minHeight: 62, fontSize: 10, color: "#fff" }}
+                      label="Today"
+                    />
+                  </Tabs>
+                </Grid>
+              </Grid>
+            </Container>
           </Box>
         </Grid>
       </Grid>
